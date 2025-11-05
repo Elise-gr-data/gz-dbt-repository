5cd1766dfc321b9d514c13076e1fa9bc74e4c74b
Elise GR <elise.gr.data@gmail.com>
2025-11-05 15:58:57 +0000

    adding margin_percent macro in 'int_sales_margin.sql' intermediate model

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,8 +1,9 @@
 SELECT
     *,
     (quantity*purchase_price) AS purchase_cost,
-    (revenue-quantity*purchase_price) AS margin
+    (revenue-quantity*purchase_price) AS margin,
+    {{margin_percent('revenue', ('quantity * purchase_price'))}} AS margin_percent
 FROM {{ref('stg_raw__sales')}}
 INNER JOIN {{ref('stg_raw__product')}}
 USING(products_id)
-ORDER BY quantity DESC,products_id
+--ORDER BY quantity DESC
